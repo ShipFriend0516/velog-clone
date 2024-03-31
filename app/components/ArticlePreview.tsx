4e4da49b7459d493858a01c5e57ba2ea77beaa35
Oracionspace <shipfriend4371@naver.com>
2024-03-31 18:47:45 +0900

    feat: 업로드 날짜 계산 기능 구현

--- app/components/ArticlePreview.tsx
+++ app/components/ArticlePreview.tsx
@@ -21,6 +21,28 @@ const ArticlePreview = ({
   likes,
   thumbnailURL,
 }: ArticlePreview) => {
+  const now = new Date().getTime();
+  const ago = Math.abs(uploadTime - now);
+  let difference = Math.floor(ago / 1000 / 60);
+  let postfix = "분 전";
+  if (difference >= 60) {
+    difference /= 60;
+    postfix = "시간 전";
+    if (difference >= 24) {
+      difference /= 24;
+      postfix = "일 전";
+      if (difference >= 30) {
+        difference /= 30;
+        postfix = "달 전";
+        if (difference >= 12) {
+          difference /= 12;
+          postfix = "년 전";
+        }
+      }
+    }
+  }
+  difference = Math.floor(difference);
+
   return (
     <div className="w-full h-96 shadow-xl rounded-sm flex flex-col justify-between">
       <div className="flex flex-col justify-between flex-grow overflow-hidden">
@@ -29,18 +51,18 @@ const ArticlePreview = ({
             className="object-cover"
             src={thumbnailURL}
             alt={title}
-            width={600}
+            width={1000}
             height={600}
             style={{ height: "50%" }}
           />
         )}
-        <div className="h-1/2">
+        <div className={`${thumbnailURL ? "h-1/2" : "h-full"} flex flex-col justify-between`}>
           <div className="p-5">
             <div className="font-bold">{title}</div>
             <p>{content}</p>
           </div>
           <div className="p-5 text-gray-500 font-light text-sm">
-            <span>{uploadTime}</span>•<span>{comments}</span>
+            <span>{difference + postfix}</span> • <span>{comments}개의 댓글</span>
           </div>
         </div>
       </div>
